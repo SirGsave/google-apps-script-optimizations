f0a0d2f7a4bc163766b372ea31eab1e6a8047416
KT.Godsave <kt.godsave@gmail.com>
2025-11-28 15:01:37 +0100

    Update optimized.js

--- nasarawa-squad-pull/optimized.js
+++ nasarawa-squad-pull/optimized.js
@@ -6,7 +6,7 @@ function pullIntoNasarawaSquad() {
     if (!sheet) throw new Error('Sheet "Nasarawa Squad" does not exist.');
 
     // === CONFIG ===
-    const sourceId = "1Kh6OkzWFT0R5gZo0K7gAhEIYzpftSIj-1AACe1k4rvk";
+    const sourceId = "1Kh6OkzWFT0R5****K7gAhEIYzpftSIj-1AACe1k4rvk";
     const sourceSheetName = "2025 Squad Export";
     const sourceRange = "E2:AI";
 
